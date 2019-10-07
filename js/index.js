688a42b0efeb5eff28c5f0657795eeab1c85b2e7
AbdiAziz-Ali <Abdizee54@gmail.com>
2019-10-07 20:17:48 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function form_open() {
+    let open = document.getElementById('login');
+    open.style.display = 'block';
+}
+function form_close() {
+    let close = document.getElementById('login');
+    close.style.display = 'none';
+}
